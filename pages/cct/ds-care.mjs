62df5fc1e3f81d937e0dc8b854d76168ab77144a
GitHub Action <action@github.com>
2024-07-22 22:36:57 +0000

    deploy: e041134a922d225c8c5ed30ea8a8ec997f3ea627

--- pages/cct/ds-care.mjs
+++ pages/cct/ds-care.mjs
@@ -1,16 +1,19 @@
 import { ref, provide, onMounted } from "vue"
-import {dsgrid, fetchDS} from "./data.mjs"
+import {dsgrid, fetch_agency_DS, fetchDS} from "./data.mjs"
 export default {
     components: {
         dsgrid,
     },
     setup() {
         const ds = ref([])
-        // const client = useClient()
-        provide('ds', ds)
+        const csi_care = ref([])
 
+        provide('ds', ds)
+        provide('csi_care', csi_care)
+        
         onMounted(async () => {
             ds.value = await fetchDS('ds_QueryCare.json');
+            csi_care.value = await fetch_agency_DS('ds_csi.json', 'QueryCare');
         })
         return {  ds }
     }
--- pages/cct/ds-provider.mjs
+++ pages/cct/ds-provider.mjs
@@ -1,16 +1,20 @@
 import { ref, provide, onMounted } from "vue"
-import {dsgrid, fetchDS} from "./data.mjs"
+import {dsgrid, fetch_agency_DS, fetchDS} from "./data.mjs"
 export default {
     components: {
         dsgrid,
     },
     setup() {
         const ds = ref([])
+        const csi_provider = ref([])
+        
         // const client = useClient()
         provide('ds', ds)
-
+        provide('csi_provider', csi_provider)
+        
         onMounted(async () => {
             ds.value = await fetchDS('ds_QueryProvider.json');
+            csi_provider.value = await fetch_agency_DS('ds_csi.json', 'QueryProvider');
         })
         return {  ds }
     }
